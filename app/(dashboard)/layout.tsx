6be1ed29c8675351911a7bcbd3dd5854b5268b57
Sung Kyu Hong <sungkyuhong93@gmail.com>
2024-02-02 17:36:33 -0500

    add update route

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,5 +1,21 @@
-const DashboardLayout = ({children}) => {
-  return <div>{children}</div>
+import { UserButton } from "@clerk/nextjs";
+
+const DashboardLayout = ({ children }) => {
+  return <div className="h-screen w-screen relative">
+    <aside className="absolute w-[200px] top-0 left-0 h-full border-r border-black/10">
+      Mood
+    </aside>
+    <div className="ml-[200px] h-full">
+      <header className="h-[60px] border-b border-black/10">
+        <div className="h-full w-full px-6 flex items-center justify-end">
+          <UserButton />
+        </div>
+        <div className="h-[calc(100vh-60px)]">
+          {children}
+        </div>
+      </header>
+    </div>
+  </div>
 }
 
 export default DashboardLayout;
---
+++ utils/api.ts
@@ -0,0 +1,28 @@
+const createUrl = (path) => {
+  return window.location.origin + path
+}
+
+export const updateEntry = async (id, content) => {
+  const res = await fetch(new Request(createUrl(`/api/journal/${id}`), {
+    method: 'PATCH',
+    body: JSON.stringify({ content })
+  }))
+
+  if(res.ok) {
+    const data = await res.json();
+    return data.data;
+  }
+}
+
+export const createNewEntry = async () => {
+  const res = await fetch(
+    new Request(createUrl('/api/journal'), {
+      method: 'POST'
+    })
+  )
+
+  if(res.ok) {
+    const data = await res.json();
+    return data.data
+  }
+}
